aeff8705b1e326216e6cc1a7512e760ec388dd06
Cloudwowx <management@cloudwowx.com>
2026-06-15 17:59:08 +0200

    x

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,10 +3,4 @@ import os
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-INTERP = os.path.expanduser("~/virtualenv/site/3.10/bin/python")
-if sys.executable != INTERP:
-    import subprocess
-    subprocess.call([INTERP, __file__] + sys.argv[1:])
-    sys.exit(0)
-
 from app import app as application
