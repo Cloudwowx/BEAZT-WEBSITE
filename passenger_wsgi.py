0a0753a580ccf8c7095e38006ad3154c1db31fa5
Cloudwowx <management@cloudwowx.com>
2026-06-15 17:41:49 +0200

    x
    x

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-INTERP = os.path.expanduser("~/virtualenv/public_html/3.10/bin/python")
+INTERP = os.path.expanduser("~/virtualenv/site/3.10/bin/python")
 if sys.executable != INTERP:
     import subprocess
     subprocess.call([INTERP, __file__] + sys.argv[1:])
